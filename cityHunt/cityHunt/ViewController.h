f2f72c8b32ee1cbc405db2114754a7a480dde36b
Baffin <530275768@qq.com>
2016-03-28 10:17:29 +0800

    init project
    cityHunt detailPage

---
+++ cityHunt/cityHunt/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  cityHunt
+//
+//  Created by 姜饼不甜 on 16/3/28.
+//  Copyright © 2016年 姜饼不甜. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ cityHunt/cityHunt/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  cityHunt
+//
+//  Created by 姜饼不甜 on 16/3/28.
+//  Copyright © 2016年 姜饼不甜. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
